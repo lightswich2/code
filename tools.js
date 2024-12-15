fe72b7af6e3cd77dc2eefcd458b8418ca6411f12
lightswich2 <ergasdos000@edmonds15.org>
2024-12-14 22:23:58 -0800

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,9 +1,11 @@
 /// tools.js
 document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		function () { var script = document.createElement('script');
-    script.src='//cdn.jsdelivr.net/npm/eruda';
-    document.body.appendChild(script);
-    script.onload = function () { eruda.init() } }
-	}
+    if (e.key == "~" && e.ctrlKey) {
+        (function() {
+            var script = document.createElement('script');
+            script.src = '//cdn.jsdelivr.net/npm/eruda';
+            document.body.appendChild(script);
+            script.onload = function() { eruda.init(); };
+        })();
+    }
 });
