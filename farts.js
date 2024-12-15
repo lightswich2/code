8d9d845c13ed9e5056611d74ccbc5e367d826ac4
lightswich2 <ergasdos000@edmonds15.org>
2024-12-14 22:55:56 -0800

    Update farts.js

--- farts.js
+++ farts.js
@@ -1,17 +1,11 @@
 /// farts.js
 document.addEventListener("keydown", function (e) {
     if (e.key === "~" && e.ctrlKey) {
-        alert("Ctrl + ~ detected. Loading Eruda...");
         var script = document.createElement('script');
         script.src = 'https://cdn.jsdelivr.net/npm/eruda';
         document.body.appendChild(script);
         script.onload = function() {
-            alert("Eruda script loaded.");
             eruda.init();
-            alert("Eruda initialized.");
-        };
-        script.onerror = function() {
-            alert("Failed to load Eruda script.");
         };
     }
 });
