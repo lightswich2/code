4e75637dd54ea82449376add08737f20c61640fa
lightswich2 <ergasdos000@edmonds15.org>
2024-12-14 17:52:49 -0800

    Create tools.js

---
+++ tools.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown", function (e) {
+	if (e.key == "~" && e.ctrlKey) {
+function () { var script = document.createElement('script'); script.src='//cdn.jsdelivr.net/npm/eruda'; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+			});
+	}
+});
