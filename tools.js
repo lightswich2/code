1306127e6bc37c67ca7bd8fe6762e391a617a723
lightswich2 <ergasdos000@edmonds15.org>
2024-12-14 18:09:15 -0800

    Update tools.js

--- tools.js
+++ tools.js
@@ -3,7 +3,7 @@ window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
     (function() {
       var script = document.createElement('script');
-      script.src = '//cdn.jsdelivr.net/npm/eruda';
+      script.src = 'cdn.jsdelivr.net/npm/eruda';
       document.body.appendChild(script);
       script.onload = function() { eruda.init() };
     })();
