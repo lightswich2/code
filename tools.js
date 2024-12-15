b380f5b35bac12f55b6a1171c81df2d44a161e4c
lightswich2 <ergasdos000@edmonds15.org>
2024-12-14 18:07:37 -0800

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,9 +1,12 @@
 /// tools.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-	function () { var script = document.createElement('script');
-	script.src='//cdn.jsdelivr.net/npm/eruda'; 
-	document.body.appendChild(script);
-	script.onload = function () { eruda.init() } }();
+    (function() {
+      var script = document.createElement('script');
+      script.src = '//cdn.jsdelivr.net/npm/eruda';
+      document.body.appendChild(script);
+      script.onload = function() { eruda.init() };
+    })();
   }
-})
+});
+
