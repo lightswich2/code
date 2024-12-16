6781daaf12b8a2956dbf3b7508566cdb58c349b9
lightswich2 <ergasdos000@edmonds15.org>
2024-12-15 17:33:31 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,8 +1,10 @@
 // auto.js
 (function() {
+  alert("loaded");
   var script = document.createElement('script');
   script.src = '//cdn.jsdelivr.net/npm/eruda';
   document.body.appendChild(script);
+  alert("2nd load");
   script.onload = function() {
     eruda.init();
   };
